ce4a492ba9cec4c886152c172afa39b9f5c9ff11
Sabareesh001 <hatrickda@gmail.com>
2025-03-14 11:36:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <div className="card">
+        <h1>Under Maintanence ⚙️</h1>
+        <h1>Linked in → <a href='https://linkedin.com/in/sabareesh001'>here</a></h1>
+        </div>
+    </>
+  )
+}
+
+export default App
